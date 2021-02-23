9664448b61a1fea1ec6f25b2778aceb09e6369b1
francesco marinelli <francescomarinelli04@gmail.com>
2021-02-23 12:31:07 +0100

    Teoria Equazione

--- EquazioniLibrary.Test/UnitTest1.cs
+++ EquazioniLibrary.Test/UnitTest1.cs
@@ -10,57 +10,81 @@ namespace EquazioniLibrary.Test
         public void GestioneTest()
         {
             double a = 0;
-            bool resp = Equazioni.IsDetermined(a);
+            bool respettivo = Equazioni.IsDetermined(a);
             bool risultato_aspettato = false;
-            Assert.AreEqual(risultato_aspettato, resp);
+            Assert.AreEqual(risultato_aspettato, respettivo);
         }
         [TestMethod]
         public void IsDeterminedTest2()
         {
             double a = 1;
-            bool resp = Equazioni.IsDetermined(a);
+            bool respettivo = Equazioni.IsDetermined(a);
             bool risultato_aspettato = true;
-            Assert.AreEqual(risultato_aspettato, resp);
+            Assert.AreEqual(risultato_aspettato, respettivo);
         }
         [TestMethod]
         public void IsInconsisted1()
         {
             double a = 1, b = 1;
-            bool resp = Equazioni.IsInconstisted(a, b);
+            bool respettivo = Equazioni.IsInconstisted(a, b);
             bool risultato_aspettato = true;
-            Assert.AreEqual(risultato_aspettato, resp);
+            Assert.AreEqual(risultato_aspettato, respettivo);
         }
         [TestMethod]
         public void IsInconsisted2()
         {
             double a = 1, b = 0;
-            bool resp = Equazioni.IsInconstisted(a, b);
+            bool respettivo = Equazioni.IsInconstisted(a, b);
             bool risultato_aspettato = true;
-            Assert.AreEqual(risultato_aspettato, resp);
+            Assert.AreEqual(risultato_aspettato, respettivo);
         }
         [TestMethod]
         public void IsInconsisted3()
         {
             double a = 0, b = 1;
-            bool resp = Equazioni.IsInconstisted(a, b);
+            bool respettivo = Equazioni.IsInconstisted(a, b);
             bool risultato_aspettato = false;
-            Assert.AreEqual(risultato_aspettato, resp);
+            Assert.AreEqual(risultato_aspettato, respettivo);
         }
         [TestMethod]
         public void IsDegree2_1()
         {
             double x = 1, esponente = 1;
-            bool resp = Equazioni.IsDegree2(esponente, x);
+            bool respettivo = Equazioni.IsDegree2(esponente, x);
             bool risultato_aspettato = false;
-            Assert.AreEqual(risultato_aspettato, resp);
+            Assert.AreEqual(risultato_aspettato, respettivo);
         }
         [TestMethod]
         public void IsDegree2_2()
         {
             double x = 1, esponente = 2;
-            bool resp = Equazioni.IsDegree2(esponente, x);
+            bool respettivo = Equazioni.IsDegree2(esponente, x);
             bool risultato_aspettato = true;
-            Assert.AreEqual(risultato_aspettato, resp);
+            Assert.AreEqual(risultato_aspettato, respettivo);
+        }
+        [TestMethod]
+        public void Delta()
+        {
+            double a = 3, b = 3, c = 4;
+            double respettivo = Equazioni.Delta(a, b, c);
+            double risultato_aspettato = -39;
+            Assert.AreEqual(risultato_aspettato, respettivo);
+        }
+        [TestMethod]
+        public void SoluzioniEquazione1Grade1()
+        {
+            double a = 0, b = 0;
+            string respettivo = Equazioni.SoluzioneEq1Grado(a, b);
+            string risultato_aspettato = "Indeterminato";
+            Assert.AreEqual(risultato_aspettato, respettivo);
+        }
+        [TestMethod]
+        public void SoluzioniEquazione1Grade2()
+        {
+            double a = 1, b = 0;
+            string respettivo = Equazioni.SoluzioneEq1Grado(a, b);
+            string risultato_aspettato = "Impossibile";
+            Assert.AreEqual(risultato_aspettato, respettivo);
         }
     }
 }
--- EquazioniLibrary/Equazioni.cs
+++ EquazioniLibrary/Equazioni.cs
@@ -1,4 +1,5 @@
-﻿namespace EquazioniLibrary
+﻿using System;
+namespace EquazioniLibrary
 {
     public class Equazioni
 
@@ -23,7 +24,35 @@
         {
             return esponente == 2 && a != 0;
         }
+        public static double Delta(double a, double b, double c)
+        {
+            return (b * b) - (4 * a * c);
+        }
+        public static string SoluzioniEquazione2Grado(double a, double b, double c)
+        {
+            
+            double delta = Delta(a, b, c);
+            double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+            if (IsDetermined(a) == true)
+                return "impossibile";
+            if (delta < 0)
+                return "impossibile";
+            return $"x1= {x1}; x2= {x2}";
+            
+        }
+        public static string SoluzioneEq1Grado(double a, double b)
+        {
 
+            if (a == 0 && b == 0)
+                return "Indeterminato";
+            else if (IsInconstisted(a, b) == true)
+                return "Impossibile";
+            else
+            {
+                double x = b / a;
+                return $"x= {x}";
+            }
+        }
     }
-  
 }
